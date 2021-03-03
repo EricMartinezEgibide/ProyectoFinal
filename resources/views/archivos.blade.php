50d81bfbb609a5c6de2e467e934d47c6b5ee2d29
lucisamy <luciasara.mercado@ikasle.egibide.org>
2021-03-03 12:41:42 +0100

    cambios estilos bootstrap

--- resources/views/archivos.blade.php
+++ resources/views/archivos.blade.php
@@ -5,9 +5,11 @@
         <form id="formMultimedia" action="{{route('multimedia.guardar',$proyecto->id)}}" method="post"
               enctype="multipart/form-data">
             {{csrf_field()}}
-            <div class="d-flex mb-3">
-                <div id="select-archivo" class="p-1 mr-3">
-                    <input type="file" name="archivo" id="archivo">
+            <div class="d-flex mb-3 flex-wrap">
+                <div id="select-archivo" class="p-1 mr-3 mw-100">
+                    <input type="file" name="archivo" id="archivo" class="mw-100 d-none d-sm-inline-block" required>
+                    <input type="button" value="Seleccionar archivo"
+                           class="d-inline-block d-sm-none" onclick="document.getElementById('archivo').click();" />
                     <span id="iconoArchivo" class="fa"></span>
                 </div>
 
@@ -17,23 +19,23 @@
             </div>
         </form>
         <!--MODAL PARA SOBREESCRIBIR-->
-<!--            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            ...
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                        </div>
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
                     </div>
                 </div>
-            </div>-->
+            </div>
+        </div>
         <!-- TABLA CON LOS ARCHIVOS -->
         <div>
             <p class="h3">Archivos de {{$proyecto->titulo}}</p>
